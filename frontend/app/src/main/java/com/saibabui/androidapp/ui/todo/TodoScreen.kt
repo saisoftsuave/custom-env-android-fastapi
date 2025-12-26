ac06042e4ba4a54361d22a037edcca4c723f2ae6
saisoftsuave <sai.babu@softsuave.org>
2025-12-26 17:26:38 +0530

    Auto-deploy update: https://462ada24adce.ngrok-free.app

--- frontend/app/src/main/java/com/saibabui/androidapp/ui/todo/TodoScreen.kt
+++ frontend/app/src/main/java/com/saibabui/androidapp/ui/todo/TodoScreen.kt
@@ -1,5 +1,6 @@
 package com.saibabui.androidapp.ui.todo
 
+import android.util.Log
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
